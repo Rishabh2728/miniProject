2511c00f531eb79ef435a71e28f217618574b584
Rishabh2728 <rishabh27kumar2006@gmail.com>
2026-01-30 09:36:51 +0530

    search Feature

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.getElementById('productContainer');
+
+fetch('https://dummyjson.com/products')
+  .then(res => res.json())
+  .then(data => {
+    const products = data.products;
+
+    products.forEach(product => {
+      const card = document.createElement('div');
+      card.className = 'product-card';
+
+      card.innerHTML = `
+            <img src="${product.thumbnail}" alt="${product.title}">
+            <h3>${product.title}</h3>
+            <p class="price">₹ ${product.price}</p>
+          `;
+
+      container.appendChild(card);
+    });
+  })
+  .catch(err => console.log(err));
+
+/////////////////////////////////////////////////////////////Search feature//////////////////////////////////////////
+const btn = document.getElementById('btn');
+const searchInput = document.getElementById('searchInput');
+
+btn.addEventListener('click', () => {
+  const query = searchInput.value.trim();
+  if (!query) return;
+
+  //redirect with query params
+  window.location.href = `search.html?q=${encodeURIComponent(query)}`;
+  searchInput.value = '';
+});
